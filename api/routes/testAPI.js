c689e54c4b71bd5a6a32d15170168b12c5474daa
abechoi <abraham.choi@icloud.com>
2020-07-22 15:55:47 -0700

    Send and Receive a JSON

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const items = {
+  "items": [
+    { "id": 1, "name": "Apples", "price": "$2" },
+    { "id": 2, "name": "Peaches", "price": "$5" }
+  ]
+}
+
 router.get('/', (req, res, next) => {
-  res.send("Connected to API...");
+  res.send(items);
 });
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
 
-  const [ apiResponse, setApiResponse ] = useState('');
-
-  const callAPI = () => {
-    fetch("http://localhost:9000/testAPI")
-    .then(res => res.text())
-    .then(res => setApiResponse(res))
-    .catch(err => err);
-  }
+  const [ error, setError ] = useState(null);
+  const [ isLoaded, setIsLoaded ] = useState(false);
+  const [ items, setItems ] = useState([]);
 
   useEffect(() => {
-    callAPI();
-  });
+    fetch("http://localhost:9000/testAPI")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+        setItems(result.items);
+    },
+    (error) => {
+      setIsLoaded(true);
+      setError(error);
+    })
+  }, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">Welcome Abe!</h1>
-      </header>
-      <p className="App-intro">{apiResponse}</p>
-    </div>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+  else if (!isLoaded) {
+    return <div>Loading...</div>
+  }
+  else{
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name} {item.price}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default App;
